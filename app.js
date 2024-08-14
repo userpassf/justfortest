54042ffe449c028a0a4096293bea86838b79b85c
userpassf <kaleabbiruk2024@gmail.com>
2024-08-15 01:52:02 +0300

    second commit

--- app.js
+++ app.js
@@ -1,10 +1,17 @@
 const express = require('express');
 const path = require('path');
 const app = express();
+const { Telegraf, Markup } = require('telegraf');
 const PORT = process.env.PORT || 3000;
+const bot = new Telegraf('6841562964:AAEs5ivXTR3VLUn15wbOemNG_LJqR92TbBU');
+
+bot.start((ctx)=>{
+ctx.reply('hello i am working');
+});
+
 
 // Serve static files
-app.use(express.static(path.join(__dirname, 'public')));
+
 app.get('/',function(req,res){
   res.send('web page is running');
 })
@@ -12,3 +19,5 @@ app.get('/',function(req,res){
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
+
+bot.launch();
